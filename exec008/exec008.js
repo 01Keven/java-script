435952367195d1776c023e186720e30dfe68329f
Keven Christian <113730071+01Keven@users.noreply.github.com>
2022-09-29 17:02:51 -0300

    aprendendo switch

---
+++ exec008/exec008.js
@@ -0,0 +1,5 @@
+var vel = 70
+console.log(` a velocidade é ${vel}km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Voce ultrapassou a velocidade permitida`)
+}
---
+++ exec008/exec009.js
@@ -0,0 +1,6 @@
+var pais = 'Brasil'
+if (pais ==  'Brasil'){
+    console.log('voce é br')
+}else {
+    console.log('voce é estrangeiro')
+}
